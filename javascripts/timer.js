5a6dceab32e5f97eb8ddc72bb249535c5f09e262
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2015-03-15 18:39:50 +0100

    added dynamic heading

---
+++ javascripts/timer.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	var index = 0
+	var tags = ['coding', 'tech', 'Javascript', 'beer', 'haskell', 'traveling', 'books']
+	window.setInterval(function () {
+		$('#variable').html(tags[index])
+		index++
+		if (index == tags.length) index = 0
+	}, 1000)
+})
