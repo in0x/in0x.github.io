a247d1cbc59296a3873b0964bdf9e25eb13a2bd7
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2015-03-17 18:31:43 +0100

    minor tweaks

--- javascripts/timer.js
+++ javascripts/timer.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	var index = 0
-	var tags = ['coding', 'tech', 'Javascript', 'beer', 'haskell', 'traveling', 'books']
+	var tags = ['coding', 'tech', 'Javascript', 'traveling', 'books']
 	window.setInterval(function () {
 		$('#variable').html(tags[index])
 		index++
 		if (index == tags.length) index = 0
-	}, 1500)
+	}, 2000)
 })
